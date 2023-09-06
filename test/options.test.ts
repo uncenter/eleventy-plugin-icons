3f460069f06049945a920bdfe23bac1526b34e46
uncenter <47499684+uncenter@users.noreply.github.com>
2023-09-06 14:47:26 -0400

    test(options): more tests

--- test/options.test.ts
+++ test/options.test.ts
@@ -6,8 +6,30 @@ test('default options are valid', () => {
 	try {
 		validateOptions(defaultOptions);
 		valid = true;
-	} catch (e) {
-		throw e;
-	}
+	} catch {}
 	expect(valid).toBe(true);
+});
+
+test('empty options are invalid', () => {
+	let valid;
+	try {
+		// @ts-ignore
+		validateOptions({});
+		valid = true;
+	} catch {
+		valid = false;
+	}
+	expect(valid).toBe(false);
+});
+
+test('invalid options throw error', () => {
+	let valid;
+	try {
+		// @ts-ignore
+		validateOptions({ ...defaultOptions, mode: false });
+		valid = true;
+	} catch {
+		valid = false;
+	}
+	expect(valid).toBe(false);
 });
