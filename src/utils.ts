7f1d50388ed1c30031a63c08949685d28e1c39f3
uncenter <47499684+uncenter@users.noreply.github.com>
2023-09-25 11:41:02 -0400

    feat: oneline json stringify, 11ty-like logging

--- src/utils.ts
+++ src/utils.ts
@@ -1,8 +1,45 @@
 import { XMLBuilder, XMLParser } from 'fast-xml-parser';
-import { Logger } from 'loogu';
+import kleur from 'kleur';
 import type { Attributes } from './types';
 
-export const log = new Logger('eleventy-plugin-icons');
+export const log = {
+	log(msg: string) {
+		message(msg);
+	},
+
+	info(msg: string) {
+		message(msg, 'warn', 'blue');
+	},
+
+	warn(msg: string) {
+		message(msg, 'warn', 'yellow');
+	},
+
+	error(msg: string) {
+		message(msg, 'error', 'red');
+	},
+};
+
+/**
+ * Formats the message to log.
+ *
+ * @param message The raw message to log.
+ * @param type The error level to log.
+ */
+function message(
+	message: string,
+	type: 'log' | 'warn' | 'error' = 'log',
+	color?: string,
+) {
+	const prefix = '[eleventy-plugin-icons] ';
+	message = `${prefix}${message.split('\n').join(`\n${prefix}`)}`;
+	if (color) {
+		// @ts-expect-error
+		console[type](kleur[color](message));
+	} else {
+		console[type](message);
+	}
+}
 
 /**
  * Merges specified attributes from an array of attribute objects and overwrite the rest.
@@ -123,4 +160,66 @@ export function parseSVG(
 	if (!svg) log.error('No SVG element found.');
 
 	return builder.build(parsed);
+}
+
+// https://github.com/fabiospampinato/json-oneline-stringify
+
+// The MIT License (MIT)
+
+// Copyright (c) 2023-present Fabio Spampinato
+
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+export function stringify(input: unknown): string | undefined {
+	const type = typeof input;
+
+	if (type === 'string' || type === 'number') return JSON.stringify(input);
+	if (type === 'boolean') return input ? 'true' : 'false';
+	if (type === 'object') {
+		if (input === null) {
+			return 'null';
+		} else if (Array.isArray(input)) {
+			let children = '';
+
+			for (let i = 0, l = input.length; i < l; i++) {
+				const value = input[i];
+				const child = stringify(value);
+
+				if (child === undefined) continue;
+
+				children += children ? `, ${child}` : child;
+			}
+
+			return `[${children}]`;
+		} else {
+			let children = '';
+
+			for (const prop in input) {
+				const value = stringify(input[prop as keyof typeof input]);
+
+				if (value === undefined) continue;
+
+				const key = stringify(prop);
+
+				children += children ? `, ${key}: ${value}` : `${key}: ${value}`;
+			}
+
+			return children ? `{ ${children} }` : '{}';
+		}
+	}
 }
