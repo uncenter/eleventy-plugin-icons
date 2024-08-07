6ad39f1bf9f57ba5451f54c5c50d78951502b3a7
uncenter <47499684+uncenter@users.noreply.github.com>
2024-08-07 10:33:09 -0400

    fix(types): make all optional options properties optional (#110)

--- src/types.ts
+++ src/types.ts
@@ -2,3 +2,14 @@
  * An object containing attribute-key pairs of strings.
  */
 export type Attributes = Record<string, string>;
+
+// eslint-disable-next-line @typescript-eslint/no-unsafe-function-type
+export type DeepPartial<T> = T extends Function | Array<any>
+	? T
+	: T extends object
+		? { [K in keyof T]?: DeepPartial<T[K]> }
+		: T | undefined;
+
+export type Prettify<T> = {
+	[K in keyof T]: T extends object ? Prettify<T[K]> : T[K];
+} & {};
