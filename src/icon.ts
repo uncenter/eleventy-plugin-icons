b9b0c2ab983edb8f5313d7bdbe6a3ef378e62745
Christian Dannie Storgaard <1196197+Cybolic@users.noreply.github.com>
2025-08-05 17:45:04 +0300

    refactor: remove redundant icon validity check in options.sprite.extraIcons

--- src/icon.ts
+++ src/icon.ts
@@ -122,18 +122,13 @@ export const getExtraIcons = async (options: Options): Promise<Icon[]> => {
 		}
 
 		for (const icon of options.sprite.extraIcons.icons) {
-			if (!icon.name || !icon.source)
-				log.error(
-					`options.sprite.extraIcons.icons: Invalid icon: ${JSON.stringify(
-						icon,
-					)}.`,
-				);
 			if (sources.some((source) => source.name === icon.source)) {
 				log.warn(
 					`options.sprite.extraIcons.icons: icons from source '${icon.source}' already included from options.sprite.extraIcons.sources: ${JSON.stringify(
 						icon,
 					)}.`,
 				);
+				continue;
 			} else {
 				icons.push(new Icon(icon, options));
 			}
