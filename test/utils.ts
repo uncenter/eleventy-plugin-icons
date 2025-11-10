724f3cdb0719f07af8281a934bcd9b3d80857e0a
uncenter <uncenter@uncenter.dev>
2025-11-10 15:19:24 -0600

    test: add benchmarks for example setups

--- test/utils.ts
+++ test/utils.ts
@@ -4,9 +4,11 @@ import { fileURLToPath } from 'node:url';
 const __dirname = fileURLToPath(new URL('.', import.meta.url));
 
 export const TEST_DIR = __dirname;
-export const FIXTURE_DIR = join(__dirname, 'fixtures/');
+export const FIXTURE_DIR = join(TEST_DIR, 'fixtures/');
+export const EXAMPLE_DIR = join(TEST_DIR, '../examples');
 
 export const withFixture = (name: string) => join(FIXTURE_DIR, name);
+export const withExample = (name: string) => join(EXAMPLE_DIR, name);
 export const getFixtureFromURL = (
 	results: Array<EleventyPageResult>,
 	url: string,
