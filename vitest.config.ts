b0766b149c0a3e8a5138980c0eab2e369f69522e
uncenter <uncenter@uncenter.dev>
2025-12-04 16:58:57 -0600

    chore(vitest): add codspeed plugin to config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vitest/config';
 
+import codspeedPlugin from '@codspeed/vitest-plugin';
+
 export default defineConfig({
+	plugins: [codspeedPlugin()],
 	test: {
 		include: ['test/*.test.ts'],
 	},
