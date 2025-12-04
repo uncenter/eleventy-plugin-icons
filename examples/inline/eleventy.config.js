bd62c9fbe0750b55851b4fa5191def13c0e7a3ba
uncenter <uncenter@uncenter.dev>
2025-12-04 16:27:22 -0600

    docs(examples): add basic inline example
    Also to be used for testing purposes...

---
+++ examples/inline/eleventy.config.js
@@ -0,0 +1,9 @@
+import pluginIcons from 'eleventy-plugin-icons';
+
+export default function (eleventyConfig) {
+	eleventyConfig.addGlobalData('layout', 'layout.njk');
+	eleventyConfig.addPlugin(pluginIcons, {
+		mode: 'inline',
+		sources: [{ name: 'lucide', path: 'node_modules/lucide-static/icons' }],
+	});
+}
